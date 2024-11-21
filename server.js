64a242f329c0ab7986550c62b49c38f34425fbee
kaustubhduse <202251045@iiitvadodara.ac.in>
2024-11-22 01:36:14 +0530

    Changes

--- server.js
+++ server.js
@@ -7,10 +7,15 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.get('/', (req, res) => {
+    res.send('School Finder API');
+})
+
 // Routes
 app.use('/api/schools', schoolRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
+    
 });
